80720026824a7cbf9d5886709f8fc12493f24048
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-10-03 19:23:26 +0300

    chore: added the fonts colors and a bit of ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex justify-center items-center flex-col bg-bg-color h-screen w-full">
+      <h1 className="font-kumbh-sans text-font-color font-bold text-3xl">pomodoro</h1>
+      <div className="flex justify-around items-center bg-ui-components rounded-full p-2 text-font-color font-bold w-96">
+        <h2 className="bg-orange-timer p-4 rounded-full text-ui-components">pomodoro</h2>
+        <h2>short break</h2>
+        <h2>long break</h2>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto-slab' : ['Roboto Slab', 'sans-serif'],
+        'kumbh-sans' : ['Kumbh Sans', 'sans-serif'],
+        'space-mono' : ['Space Mono', 'monospace']
+      },
+      colors: {
+        'bg-color' : 'var(--background-color)',
+        'orange-timer' : 'var(--orange-timer)',
+        'blue-timer' : 'var(--blue-timer)',
+        'purple-timer' : 'var(--purple-timer)',
+        'font-color' : 'var(--font-color)',
+        'ui-components' : 'var(--ui-components)',
+        'menu-color' : 'var(--menu-color)',
+        'settings-components' : 'var(--setings-components)'
+      }
+    },
   },
   plugins: [],
 };
